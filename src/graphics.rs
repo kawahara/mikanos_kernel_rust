732f1122cab967657d7f46f4a9a9286def59d17a
Shogo Kawahara <kawahara@bucyou.net>
2022-06-27 14:59:39 +0900

    Add graphics feature (osbook_day04d)

---
+++ src/graphics.rs
@@ -0,0 +1,66 @@
+#[derive(Debug, Copy, Clone)]
+#[repr(u32)]
+pub enum PixelFormat {
+    Rgb = 0,
+    Bgr,
+}
+
+#[derive(Debug, Copy, Clone)]
+#[repr(C)]
+pub struct FrameBuffer {
+    pub frame_buffer: *mut u8,
+    pub pixels_per_scan_line: u32,
+    pub horizontal_resolution: u32,
+    pub vertical_resolution: u32,
+    pub format: PixelFormat,
+}
+
+impl FrameBuffer {
+    pub unsafe fn write_byte(&mut self, index: usize, val: u8) {
+        self.frame_buffer.add(index).write_volatile(val)
+    }
+
+    pub unsafe fn write_value(&mut self, index: usize, value: [u8; 3]) {
+        (self.frame_buffer.add(index) as *mut [u8; 3]).write_volatile(value)
+    }
+}
+
+
+pub struct PixelColor(pub u8, pub u8, pub u8);
+
+pub struct Graphics {
+    fb: FrameBuffer,
+    pixel_writer: unsafe fn(&mut FrameBuffer, usize, &PixelColor),
+}
+
+impl Graphics {
+    pub fn new(fb: FrameBuffer) -> Self {
+        unsafe fn write_pixel_rgb(fb: &mut FrameBuffer, index: usize, rgb: &PixelColor) {
+            fb.write_value(index, [rgb.0, rgb.1, rgb.2]);
+        }
+        unsafe fn write_pixel_bgr(fb: &mut FrameBuffer, index: usize, rgb: &PixelColor) {
+            fb.write_value(index, [rgb.2, rgb.1, rgb.0]);
+        }
+        let pixel_writer = match fb.format {
+            PixelFormat::Rgb => write_pixel_rgb,
+            PixelFormat::Bgr => write_pixel_bgr
+        };
+
+
+        Graphics { fb, pixel_writer }
+    }
+
+    pub fn write_pixel(&mut self, x: usize, y: usize, color: &PixelColor) {
+        if x > self.fb.horizontal_resolution as usize {
+            panic!("bad x coord");
+        }
+        if y > self.fb.vertical_resolution as usize {
+            panic!("bad y coord");
+        }
+        let pixel_index = y * (self.fb.pixels_per_scan_line as usize) + x;
+        let base = 4 * pixel_index;
+        unsafe {
+            (self.pixel_writer)(&mut self.fb, base, &color);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #![no_std]
 #![no_main]
 
-use core::arch::asm;
+pub mod graphics;
+
 use core::panic::PanicInfo;
+use core::arch::asm;
+use graphics::{Graphics, FrameBuffer, PixelColor};
 
 #[no_mangle]
-extern "C" fn kernel_main() {
+extern "C" fn kernel_main(fb: *mut FrameBuffer) {
+    let fb_a = unsafe {*fb};
+    let mut graphics = Graphics::new(fb_a);
+
+    for x in 0..fb_a.horizontal_resolution as usize {
+        for y in 0..fb_a.vertical_resolution as usize {
+            graphics.write_pixel(x, y, &PixelColor(255, 255, 255));
+        }
+    }
+
+    for x in 0..200 {
+        for y in 0..100 {
+            graphics.write_pixel(x, y, &PixelColor(0, 255, 0))
+        }
+    }
+
     unsafe {
         loop {
             asm!("hlt");
