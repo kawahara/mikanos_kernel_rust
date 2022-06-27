35911ff93b1957ee343f2dbd768c7eb48e25bcdf
Shogo Kawahara <kawahara@bucyou.net>
2022-06-27 15:05:05 +0900

    Fix format

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub mod graphics;
 use core::panic::PanicInfo;
 use graphics::{Graphics, FrameBuffer, PixelColor};
 
-fn hlt_loop () -> ! {
+fn hlt_loop() -> ! {
     loop {
         x86_64::instructions::hlt();
     }
@@ -14,7 +14,7 @@ fn hlt_loop () -> ! {
 
 #[no_mangle]
 extern "C" fn kernel_main(fb: *mut FrameBuffer) {
-    let fb_a = unsafe {*fb};
+    let fb_a = unsafe { *fb };
     let mut graphics = Graphics::new(fb_a);
 
     for x in 0..fb_a.horizontal_resolution as usize {
@@ -31,7 +31,6 @@ extern "C" fn kernel_main(fb: *mut FrameBuffer) {
 
     hlt_loop();
 }
-
 
 
 #[panic_handler]
